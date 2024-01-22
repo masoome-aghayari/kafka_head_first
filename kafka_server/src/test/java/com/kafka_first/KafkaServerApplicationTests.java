c373610c3b9e6f96efaf3df4442b080d51743d2d
masoome_aghayari <masoome.aghayari@gmail.com>
2024-01-22 23:07:09 +0330

    kafka server initial commit

---
+++ kafka_server/src/test/java/com/kafka_first/KafkaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
